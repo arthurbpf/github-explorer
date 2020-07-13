05c01cf218bee4c5cb7a118e38411b69e32976b5
Arthur Fernandes <arthurbpfernandes@gmail.com>
2020-07-13 18:57:59 -0300

    Finished Dashboard page design

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+
+import { Form, Title, Repositories } from './styles';
+
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="github_explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form>
+        <input type="text" placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="" alt="Profile" />
+          <div>
+            <strong>repository/name</strong>
+            <p>descricao</p>
+          </div>
+
+          <FiChevronRight size={30} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
